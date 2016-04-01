6037dae72d3fded230e7864c50e722ab4a282eb2
Sara Bee <sara.bainbridge@gmail.com>
2016-04-01 19:49:07 -0400

    Only print suns on lux change

--- plantokei.ino
+++ plantokei.ino
@@ -3,6 +3,7 @@
 #define cs   6
 #define dc   7
 #define rst  8  // you can also connect this to the Arduino reset
+#define photocell 0
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library
@@ -18,6 +19,8 @@ Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, mosi, sclk, rst);
 
 float p = 3.1415926;
 
+bool SUN_FILLS[] = {false, false, false, false, false};
+
 int SUN_RADIUS = 5;
 int SUN_PADDING = 2;
 
@@ -27,32 +30,54 @@ int LINE_SUNS = 10 * LINE_HEIGHT;
 
 void setup(void) {
   Serial.begin(9600);
-  Serial.print("hello!");
+  Serial.println("hello!");
   
   tft.initR(INITR_BLACKTAB); 
   tft.fillScreen(ST7735_BLACK);
+  initSuns();
 }
 
 void loop() {
-  printLumens();
-  delay(500);
+  int lightLevel = getLightLevel();
+  printLumens(lightLevel);
+  delay(100);
 }
 
-void printLumens() {
-  printSun(0, true);
-  printSun(1, true);
-  printSun(2, true);
-  printSun(3, false);
-  printSun(4, false);
+int getLightLevel() {
+  float photocellReading = (float) analogRead(photocell);
+  int lightLevel = floor((photocellReading / 1023) * 5);
+  return lightLevel;
+}
+
+void initSuns() {
+  for (int i = 1; i <= 5; i++) {
+    printSun(i, false);
+  }
+}
+
+void printLumens(int lightLevel) {
+  for (int i = 1; i <= 5; i++) {
+    bool hasFill = i <= lightLevel;
+    bool hadFill = SUN_FILLS[i-1];
+
+    if(hasFill != hadFill) {
+      printSun(i, hasFill);
+    }
+    
+    SUN_FILLS[i-1] = hasFill;
+  }
 }
 
 void printSun(int i, bool fill) {
-  int outlineX = (i * SUN_RADIUS * 2) + SUN_RADIUS + (i * SUN_PADDING);
-  int outlineY = 10 * LINE_HEIGHT;
-  tft.drawCircle(outlineX, outlineY, SUN_RADIUS, ST7735_YELLOW);
+  int j = i - 1;
+  int outlineX = 28 + (j * SUN_RADIUS * 2) + SUN_RADIUS + (j * SUN_PADDING);
 
   if (fill) {
-    tft.fillCircle(outlineX, outlineY, SUN_RADIUS, ST7735_YELLOW);
+    tft.fillCircle(outlineX, LINE_SUNS, SUN_RADIUS, ST7735_YELLOW);
+  } else {
+    tft.fillCircle(outlineX, LINE_SUNS, SUN_RADIUS, ST7735_BLACK);
   }
+
+  tft.drawCircle(outlineX, LINE_SUNS, SUN_RADIUS, ST7735_YELLOW);
 }
 
