0de900687c613135955e13a511a3762d53dc1bfe
Sara Bee <sara.bainbridge@gmail.com>
2016-04-04 17:40:41 -0400

    Move music stuff into library

--- libraries/Plantokei-Music/Plantokei_Music.h
+++ libraries/Plantokei-Music/Plantokei_Music.h
@@ -1,3 +1,8 @@
+#ifndef Plantokei_Music_h
+#define Plantokei_Music_h
+
+#include "Arduino.h"
+
 #define c 3830    // 261 Hz
 #define d 3400    // 294 Hz
 #define e 3038    // 329 Hz
@@ -7,3 +12,16 @@
 #define b 2028    // 493 Hz
 #define C 1912    // 523 Hz
 #define R 0 // Define a special note, 'R', to represent a rest
+
+class PlantokeiMusic
+{
+  public:
+    PlantokeiMusic(int pin);
+    void playTone();
+    void playSong();
+
+  private:
+    int _pin;
+};
+
+#endif
